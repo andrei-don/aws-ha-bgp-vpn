186bbcac549afb7cf9d9bd0b498b89b7eef33ade
Alex Stan <alexstan@Alexs-MBP.broadband>
2024-07-04 12:05:26 +0100

    Corrected typo in script name and added correct output for private ip address

--- outputs.tf
+++ outputs.tf
@@ -10,10 +10,10 @@ output "public_ip_router_b" {
 
 output "private_ip_router_a" {
   description = "The private IP of onprem router A"
-  value       = aws_network_interface.onprem_private_a.private_ip
+  value       = aws_network_interface.onprem_public_a.private_ip
 }
 
 output "private_ip_router_b" {
   description = "The private IP of onprem router B"
-  value       = aws_network_interface.onprem_private_b.private_ip
+  value       = aws_network_interface.onprem_public_b.private_ip
 }
